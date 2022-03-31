ccef92cf730d017abfbe65004a6ebfa66f3f2b5d
SoloviovWeb <98924242+SoloviovWeb@users.noreply.github.com>
2022-03-31 12:57:31 +0300

    push files

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// @@include('alert.js')
+
+let menuItem = document.querySelector('.menu-block__item--mobile'),
+    wrapperMenu = document.querySelector('.menu-block__wrapper');
+
+menuItem.addEventListener('click', function(){
+    if(wrapperMenu.classList.contains('open')){
+        wrapperMenu.classList.remove('open');
+    }else{
+        wrapperMenu.classList.add('open');
+    }
+})
